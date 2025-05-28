10f97771800225ce87e0fc3ac41a66b4492d476e
Dave001-web <davidshun991@gmail.com>
2025-05-28 14:20:07 +0100

    feat: added new file

--- controllers/PropertyController.js
+++ controllers/PropertyController.js
@@ -5,16 +5,69 @@ const createProperty = async (req, res) => {
 };
 
 const getLandlordsProperties = async (req, res) => {
-  res.send("get landlords property");
+  const { userId } = req.user; // Get the landlord's userId from the authenticated user
+  const { page = 1} = req.query
+  const skip = (page - 1) * limit
+  const limit = 5
+  try {
+    const properties = await PROPERTY.find({ landlord: userId })
+    .sort(-createAt)
+    .skip(skip)
+    .limit(limit)
+
+    const [total, availableProperties, rentedProperties] = await Promise.all([
+        PROPERTY.countDocuments({ landlord: userId }), // Get total number of properties for the landlord
+        PROPERTY.countDocuments({ 
+         landlord: userId, 
+         availability: "available"
+        }),
+        PROPERTY.countDocuments({
+         landlord: userId,
+         availability: "rented"
+      }),
+    ])
+
+    const totalPages = Math.ceil(total / limit); // Calculate total pages
+
+    res.status(200).json({
+        total, 
+        availableProperties, 
+        rentedProperties, 
+        properties, 
+        currentPages:parseInt(page), 
+        totalPages
+    });
+  } catch (error) {
+    console.error(error)
+    res.status(500).json({ message: error.message });
+  }
 };
 
 const updatePropertyAvailability = async (req, res) => {
-  res.send("update property availability");
+    const { propertyId } = req.params; // Get the property ID from the request parameters
+    const { availability } = req.body; // Get the new availability status from the request body
+    if (!availability || !["available", "rented"].includes(availability)) {
+        return res.status(400).json({ message: "Provide availabity" });
+    }
+    try {
+        const property = await PROPERTY.findById(propertyId);
+        property.availability = availability; // Update the availability status
+        await property.save(); // Save the updated property
+
+        res.status(200).json({ 
+            success: true, 
+            message: "Property availability updated", 
+            property 
+        });
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ message: error.message });
+    }
 };
 
 //find() // number of pages
 const getAllProperties = async (req, res) => {
-  const { page = 1 } = req.query;
+  const { page = 1, location, budget, type} = req.query;
   const limit = 12; // Number of properties per page
   const skip = (page - 1) * limit; // Calculate the number of properties to skip
   try {
@@ -23,6 +76,10 @@ const getAllProperties = async (req, res) => {
     };
     if(location) {
         filter.location = { $regex: location, $options: "i"}; // Case-insensitive search for location
+    } if (budget) {
+        filter.price = {$lte: parseInt(budget)}; // Filter properties with price less than or equal to budget
+    } if (type) {
+        filter.title = { $regex: type, $options: "i" }; // Case-insensitive search for property type in title
     }
     
     const properties = await PROPERTY.find(filter)
@@ -48,7 +105,39 @@ const getAllProperties = async (req, res) => {
 };
 
 const getAProperty = async (req, res) => {
-  res.send("get a property");
+    const { propertyId } = req.params;
+    try {
+        const property = await PROPERTY.findById(propertyId).populate("landlord", "fullName profilePicture phoneNumber email"); // Populate landlord details 
+
+        // more from landlord
+        const moreFromLandlord = await PROPERTY.find({
+            landlord: property.landlord._id, // Find properties by the same landlord
+            _id: { $ne: propertyId }, // Exclude the current property
+            availability: "available", // Only available properties
+        })
+        .limit(3)
+        .sort("-createAt"); // Limit to 3 more properties
+
+        // similar price range 20% of the properties price
+        // 1000 800 - 1200
+        const priceRange = property.price * 0.2; // 20% of the property price
+        const similarProperties = await PROPERTY.find({
+            _id: { $ne: propertyId }, // Exclude the current property
+            availability: "available", // Only available properties
+            price: {
+                $gte: property.price - priceRange, // Minimum price
+                $lte: property.price + priceRange, // Maximum price
+            },
+            location: property.location, // Same location
+        }).limit(3).sort("-createAt"); // Limit to 3 similar properties 
+
+        res.status(200).json({ property, moreFromLandlord, similarProperties });
+
+        res.status(200).json({ property, moreFromLandlord });
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ message: error.message });
+    }
 };
 
 module.exports = {
--- populatedb.js
+++ populatedb.js
@@ -1,11 +1,11 @@
 require('dotenv').config();
 const mongoose = require('mongoose');
 const PROPERTY = require('./models/property');
-const properties = require('./data/properties.json');
+const properties = require('./data.json');
 
 const populate = async () => {
     try {
-        await mongoose.connect(process.env.MONGO_URI, { dbName: 'torii-gate' });
+        await mongoose.connect(process.env.MONGO_URI, { dbName: 'toriigate' });
         await PROPERTY.deleteMany(); // Clear existing properties
         await PROPERTY.create(properties); // Insert new properties
         console.log("properties added")
